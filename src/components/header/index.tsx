841ed658103cb78027c0b1f011e4f2129f921f65
wu.hui <wu.hui@qkupower.com>
2022-05-24 15:43:43 +0800

    feat: header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-// import { Menu } from "antd"
-// import { Link } from "react-router-dom"
-// import type { MenuProps } from "antd"
+import { useEffect, useState } from 'react'
 import classnames from 'classnames'
-// import { MailOutlined, AppstoreOutlined, SettingOutlined } from "@ant-design/icons"
+import { useNavigate, useLocation } from 'react-router-dom'
 import styles from './index.module.less'
+import { Drawer, Input } from 'antd'
+import ThemeSwitch from '../ThemeSwitch'
+import { CaretRightOutlined } from '@ant-design/icons'
 
 const menu = [
   {
@@ -24,35 +24,103 @@ const menu = [
     link: '/activity',
   },
 ]
+
 function Component() {
-  const [current] = useState('/')
+  const location = useLocation()
+  const navigate = useNavigate()
+  const [current, setCurrent] = useState('/')
+  const [userInfo, setUserInfo] = useState<null | {
+    userName: string
+    userId: string
+    avatar: string
+  }>(null)
 
-  // const onClick: MenuProps["onClick"] = (e) => {
-  //   console.log("click ", e)
-  //   setCurrent(e.key)
-  // }
-  // test
+  const [drawVisible, setDrawVisible] = useState(false)
 
+  // 路由切换
+  const go = (link: string) => {
+    setCurrent(link)
+    navigate(link)
+  }
+  // 登录
+  const login = () => {
+    setUserInfo({
+      userId: '1',
+      userName: 'hahaha',
+      avatar: 'https://static.oschina.net/uploads/user/508/1017341_50.jpg',
+    })
+  }
+
+  const onSearch = (v: string) => {
+    console.log(v)
+  }
+
+  useEffect(() => {
+    setCurrent(location.pathname)
+  }, [])
   return (
-    <div className={styles['header-box']}>
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <ul className={styles.menu}>
-            {menu.map((item) => (
-              <li
-                key={item.link}
-                className={classnames(styles['menu-item'], {
-                  active: item.link === current,
-                })}
-              >
-                <span>{item.name}</span>
-              </li>
-            ))}
-          </ul>
+    <>
+      <div className={styles['header-box']}>
+        <div className={styles['header-box-wrapper']}>
+          <div className={styles['header-box-inner']}>
+            <ul className={styles['nav']}>
+              {menu.map((item) => (
+                <li
+                  key={item.link}
+                  className={classnames(styles['nav-item'], {
+                    [styles.active]: item.link === current,
+                  })}
+                >
+                  <span className={styles['nav-item-link']} onClick={() => go(item.link)}>
+                    {item.name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+            <div className={styles['search-box']}>
+              <Input.Search
+                placeholder="input search text"
+                allowClear
+                onSearch={onSearch}
+                className={styles['search-input']}
+              />
+            </div>
+            <div className={styles['theme-box']}>
+              <ThemeSwitch />
+            </div>
+            <div className={styles['user-box']}>
+              {userInfo ? (
+                <div className={styles['logined']} onClick={() => setDrawVisible(true)}>
+                  <img
+                    src={userInfo.avatar}
+                    alt={userInfo.userName}
+                    title={userInfo.userName}
+                    srcSet=""
+                    className={styles['avatar']}
+                  />
+                  <CaretRightOutlined className={styles['arrow']} />
+                </div>
+              ) : (
+                <span onClick={login} className={styles['login-btn']}>
+                  登录
+                </span>
+              )}
+            </div>
+          </div>
         </div>
-        <div className={styles.right}>登录</div>
       </div>
-    </div>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        closable={false}
+        onClose={() => setDrawVisible(false)}
+        visible={drawVisible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
   )
 }
 
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,10 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from '@/components/header'
 
 function Component() {
   return (
     <div>
       <Header />
-
-      <hr />
       <Outlet />
     </div>
   )
